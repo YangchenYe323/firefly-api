0fa974e767c862792b8849d55137f18248111f10
Yangchen Ye <eikasia30@gmail.com>
2025-08-03 12:41:08 -0400

    skip env test in CI

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -2,6 +2,11 @@ pub mod bilibili;
 
 #[cfg(test)]
 pub fn env() -> std::collections::HashMap<String, String> {
+    // Skip in CI
+    if std::env::var("CI").is_ok() {
+        return std::collections::HashMap::new();
+    }
+
     // Wasm32 has no filesystem access support. Embed the .dev.vars file into the test binary.
     let env = include_str!("../../.dev.vars");
     env.lines().map(|line| {
